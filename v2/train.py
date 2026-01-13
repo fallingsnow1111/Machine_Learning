1d37d92b0e2df4e50f825b8bbfb67c3044e4c567
王林钦 <13825890848@163.com>
2026-01-13 17:47:24 +0800

    test

--- v2/train.py
+++ v2/train.py
@@ -5,8 +5,8 @@ from ultralytics import YOLO
 # ==========================================
 # 1. 配置参数
 # ==========================================
-TRAIN_DATA = "./Data/dataset_yolo_processed/dataset.yaml"
-VAL_DATA = "./Data/dataset_yolo_processed/dataset.yaml" 
+TRAIN_DATA = "./Data/dataset_yolo/dataset.yaml"
+VAL_DATA = "./Data/dataset_yolo/dataset.yaml" 
 MODEL_CONFIG = "./yolo11P.yaml"
 PRETRAINED_WEIGHTS = "./yolo11n.pt"
 DEVICE = '0' if torch.cuda.is_available() else 'cpu'
