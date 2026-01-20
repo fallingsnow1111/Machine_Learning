4193b39cc807c328317fc24760e5ce1db522f672
ZiDXie <shengxie47@gmail.com>
2026-01-20 11:29:41 +0800

    Test.

--- src/train_without_distill.py
+++ src/train_without_distill.py
@@ -119,8 +119,8 @@ if __name__ == "__main__":
         split="test",
         imgsz=640,
         batch=8,
-        conf=0.15,               # 置信度阈值
-        iou=0.6,                 # NMS的IoU阈值
+        conf=0.25,               # 置信度阈值
+        iou=0.7,                 # NMS的IoU阈值
         plots=True,              # 生成验证图表
         save_json=True,          # 保存结果为JSON
     )
